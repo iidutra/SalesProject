b2c7095ab1823d151f2dbe2d783996839c9329c5
Igor Pereira Dutra <igordutra07@gmail.com>
2019-07-18 11:58:58 -0400

    Exception handling for delete (referencial integrity)

---
+++ SaleProjetoMVC/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace SaleProjetoMVC.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
